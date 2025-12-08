3c429825f2fec3e267917de7b665ce1e3fd63578
Povl Aage <git@povlaage.dk>
2025-12-08 18:06:07 +0100

    08 faster

--- 2025/08.py
+++ 2025/08.py
@@ -43,17 +43,28 @@ class Playground:
     circuit_boxes: dict[int, list[int]]
     circuit_count: int
     circuit_len: list[int]
+    sorted_dists: list[tuple[int, int, int]]
 
     @staticmethod
     def from_boxes(boxes: list[Box]):
         n = len(boxes)
-        return Playground(
+        playground = Playground(
             boxes=boxes,
-            box_circuit=[i for i in range(n)],
-            circuit_boxes={},
-            circuit_count=n,
-            circuit_len=[1] * n,
+            box_circuit=None,
+            circuit_boxes=None,
+            circuit_count=None,
+            circuit_len=None,
+            sorted_dists=sorted([(dist_sq(boxes[i1], boxes[i2]), i1, i2) for i1 in range(n) for i2 in range(n) if i1 < i2])
         )
+        playground.reset()
+        return playground
+
+    def reset(self):
+        n = len(self.boxes)
+        self.box_circuit=[i for i in range(n)]
+        self.circuit_boxes={}
+        self.circuit_count=len(self.boxes)
+        self.circuit_len=[1] * n
 
     def connect(self, bi1, bi2):
         assert bi1 != bi2
@@ -91,13 +102,11 @@ class Playground:
         return sorted(self.circuit_len, reverse=True)
 
     def connect_shortest_n(self: Playground, n: int):
-        sorted_dists = sorted([(dist_sq(self.boxes[i1], self.boxes[i2]), i1, i2) for i1 in range(len(self.boxes)) for i2 in range(len(self.boxes)) if i1 < i2])
-        for i1, i2 in ((i1, i2) for _, i1, i2 in sorted_dists[:n]):
+        for _, i1, i2 in self.sorted_dists[:n]:
             self.connect(i1, i2)
 
     def connect_until(self: Playground) -> tuple[Box, Box]:
-        sorted_dists = sorted([(dist_sq(self.boxes[i1], self.boxes[i2]), i1, i2) for i1 in range(len(self.boxes)) for i2 in range(len(self.boxes)) if i1 < i2])
-        for i1, i2 in ((i1, i2) for _, i1, i2 in sorted_dists):
+        for _, i1, i2 in self.sorted_dists:
             self.connect(i1, i2)
             if self.circuit_count == 1:
                 return self.boxes[i1], self.boxes[i2]
@@ -110,17 +119,21 @@ def parse(lines) -> Playground:
     return Playground.from_boxes([parse_box(line) for line in lines])
 
 def dist_sq(b1: Box, b2: Box):
-    return sum((w2-w1)**2 for w1, w2 in zip(b1, b2))
+    x1, y1, z1 = b1
+    x2, y2, z2 = b2
+    return (x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2
 
-def a(input_lines, n_connections):
-    circuit = parse(input_lines)
-    circuit.connect_shortest_n(n_connections)
-    c1, c2, c3 = circuit.get_circuits_by_size()[:3]
+def a(input_lines, n_connections, existing_playground=None):
+    playground = existing_playground or parse(input_lines)
+    playground.reset()
+    playground.connect_shortest_n(n_connections)
+    c1, c2, c3 = playground.get_circuits_by_size()[:3]
     return c1 * c2 * c3
 
-def b(input_lines):
-    circuit = parse(input_lines)
-    b1, b2 = circuit.connect_until()
+def b(input_lines, existing_playground=None):
+    playground = existing_playground or parse(input_lines)
+    playground.reset()
+    b1, b2 = playground.connect_until()
     x1, _, _ = b1
     x2, _, _ = b2
     return x1 * x2
@@ -159,7 +172,8 @@ example_circuit.connect_shortest_n(10)
 assert list(example_circuit.get_circuits_by_size())[:5] == [5, 4, 2, 2, 1], list(example_circuit.get_circuits_by_size())[:5]
 assert a(example_input, n_connections=10) == 40, a(example_input, 10)
 assert b(example_input) == 25272, b(example_input)
-verify(a(read_input_lines(), n_connections=1000), 66912)
-verify(b(read_input_lines()), 724454082)
+playground = parse(read_input_lines())
+verify(a(None, n_connections=1000, existing_playground=playground), 66912)
+verify(b(None, existing_playground=playground), 724454082)
 
 print(f"Elapsed: {time() - start_time:.3f}s")
